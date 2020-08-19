f99b4e0a6be16d19303dfe7c80b6f33d8d085a6d
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-08-19 15:33:41 +0200

    fix a bug in register

--- src/context/auth/authState.js
+++ src/context/auth/authState.js
@@ -44,6 +44,7 @@ const AuthState = props => {
                 type: REGISTER_SUCCESS,
                 payload: res.data
             });
+            loadUser();
         } catch (error) {
             console.log(error)
             dispatch({
